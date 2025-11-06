a387f9191d180da66f8d4fcb15fed1626acbc522
itwm <olegvertman@gmail.com>
2025-11-06 11:53:51 +0100

    fix

--- input_files/data_read.py
+++ input_files/data_read.py
@@ -4,7 +4,6 @@ import os
 import sys
 import os
 from datetime import datetime
-from sus_test import estimate_Oliver
 
 import matplotlib.pyplot as plt
 import pgeocode
@@ -14,11 +13,6 @@ PATH = os.getcwd()
 
 PZN_PATH = PATH + "/input_files/actual_pzn.csv"
 DETAILS_PATH = PATH + "/input_files/actualdetails_teststelle.csv"
-
-PATH = os.getcwd()
-
-PZN_PATH = PATH + "/input/actual_pzn.csv"
-DETAILS_PATH = PATH + "/input/actualdetails_teststelle.csv"
 
 MIN_DIST = 50  # km
 MAX_DIST = 100  # km
@@ -96,7 +90,7 @@ def get_time(time_test, time_result):
 
 
 def check(path):
-    with open("dateien/patienten/" + path, "r") as file:
+    with open(path, "r") as file:
         data = json.load(file)
     patienten_postleitzahl = data["patienten_postleitzahl"]
     teststelle_postleitzahl = data["teststelle"]["postleitzahl"]
@@ -120,10 +114,6 @@ def check(path):
 
 
 if __name__ == "__main__":
-    for i in range(30):
-        (estimated_distance, estimated_name, estimated_pzn, estimated_time) = check(str(i+1)+".json")
-        sus = estimate_Oliver(distance_check=estimated_distance, time_check=estimated_time, name_check=estimated_name, pzn_check=estimated_pzn)
-        print("sus level"+str(i)+": " + str(sus))
     
     # graph for time
     x = [i for i in range(int(MAX_TEST_TIME * 1.1))]
